b26a1a582467c40180f3b8c293f032a65d7f74d4
DinelMax <73960425+LIGHTNING283@users.noreply.github.com>
2021-02-22 17:40:52 +0530

    Update tools.sh

--- ToolKit/tools.sh
+++ ToolKit/tools.sh
@@ -2,10 +2,11 @@
 
 if [[ ! -f /data/.ToolKit/webviewbak/webview.apk ]]
 then
-echo "Initializing........."
-cp /system/framework/framework-res.apk /data/.ToolKit/frameworkbak/
+figlet Initializing
 cp /system/priv-app/webview/webview.apk /data/.ToolKit/webviewbak/
 cp /system/fonts/* /data/.ToolKit/customization/baks/stockfonts/fonts/
+cp -R /data/app/ch.deletescape.lawnchair.plah-1 /data/.ToolKit/customization/baks/launcher/
+cp -R /data/data/ch.deletescape.lawnchair.plah /data/.ToolKit/customization/baks/launcher/
 fi
 
 gsudo
