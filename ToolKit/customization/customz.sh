bf75fb0d6ba6991afa669b3a09a774d2673aff8b
DinelMax <73960425+LIGHTNING283@users.noreply.github.com>
2021-02-22 16:33:46 +0530

    Update customz.sh

--- ToolKit/customization/customz.sh
+++ ToolKit/customization/customz.sh
@@ -6,6 +6,7 @@ figlet Customization
 echo Need To Customize eh...?
 echo -e "\e[32m1.New Font + emojis\e[0m "
 echo -e "\e[32m2.Emojis-v2\e[0m "
+echo -e "\e[32m3.Leena Launcher\e[0m "
 read -p "choose an option : " choice
 
 if [ $choice = 1 ]
@@ -26,14 +27,14 @@ chmod +x /system/fonts/*
 reboot
 fi
 
-#if [ $choice = 4 ]
-#then
-#rm -rf /system/framework/framework-res.apk
-#cp /sdcard/Download/ToolKit/.frameworkbak/framework-res.apk /system/framework/
-#chmod 777 /system/framework/framework-res.apk
-#echo done!! Now Reboot...
-#reboot
-#fi
+if [ $choice = 3 ]
+then
+rm -rf /system/framework/framework-res.apk
+cp /sdcard/Download/ToolKit/.frameworkbak/framework-res.apk /system/framework/
+chmod 777 /system/framework/framework-res.apk
+echo done!! Now Reboot...
+reboot
+fi
 
 
 
