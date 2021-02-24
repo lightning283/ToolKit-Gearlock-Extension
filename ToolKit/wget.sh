d63b011b3592a1629b3507d5b3297da3bea501fe
DinelMax <73960425+LIGHTNING283@users.noreply.github.com>
2021-02-25 02:24:38 +0700

    Added wget script.
    *half working

---
+++ ToolKit/wget.sh
@@ -0,0 +1,109 @@
+clear
+figlet Install-PKG
+
+
+echo -e "\e[32m1.Essential-Apps--> Pack\e[0m "
+echo -e "\e[32m2.System-Tweak-Apks--> Pack\e[0m "
+echo -e "\e[32m3.Customization--> Pack\e[0m "
+echo -e "\e[32m4.Pubgm-Fix--> Pack\e[0m "
+echo -e "\e[32m4.Back\e[0m "
+read -p "choose an option : " choice
+if [ $choice = 1 ]
+
+then
+echo "
+What Does This Package Have?
+
+*Etch Droid
+*GameGaurdian
+*LuckyPatcher
+*RootUninstaller
+*Terminal-Emulator 
+
+
+"
+
+echo "Press '1' to Download...Press '2' to Go Back"
+read -p "Choose an Option : " choicewget
+if [ $choicewget = 1 ]
+then
+cd /data/.ToolKit/apks/
+wget http://download1081.mediafire.com/ua3fp177k23g/rg99c6bptj52mdu/apks.zip
+unzip apks.zip
+rm -rf apks.zip
+read -n 1 -s -r -p "Download Complete--Press Enter To Continue"
+bash /data/.ToolKit/tools.sh
+fi
+
+if [ $choicewget = 2 ]
+then 
+bash /data/.ToolKit/tools.sh
+fi
+#finish first option
+fi
+######################
+
+if [ $choice = 2 ]
+then
+echo "
+What Does This Pack Contain
+*3C-ToolBox
+*BatteryGURU
+*Kernel Auditor
+*L-Speed
+*New and Updated Webview--it may help In app crashes
+*Wifi-ToolBox
+*Greeniy
+
+
+"
+echo "Press '1' to Download...Press '2' to Go Back"
+read -p "Choose an Option : " choicewget2
+
+if [ choicewget2 = 1 ]
+then
+cd /data/.ToolKit/System-tweaks/
+wget http://download1592.mediafire.com/mp3w002m5i3g/dczbpysor5xtd30/system-tweaks.zip
+unzip system-tweaks.zip
+rm -rf system-tweaks.zip
+read -n 1 -s -r -p "Download Complete--Press Enter To Continue"
+bash /data/.ToolKit/tools.sh
+fi
+
+if [ choicewget2 = 2 ]
+then
+bash /data/.ToolKit/tools.sh
+fi
+##
+fi
+###################################################################################################
+
+if [ $choice = 3 ]
+then
+ echo "
+ What Does This Pack Have?
+ *Fonts
+ *New Launchers
+ **Much More to come here**
+
+ "
+ echo "Press '1' to Download...Press '2' to Go Back"
+ read -p "Choose an Option : " choicewget3
+ if [ $choicewget3 = 1 ]
+ then
+ cd /data/.ToolKit/customization/
+ wget http://download940.mediafire.com/bxz51oc8ogvg/qv6d3klvfa9o0fn/customization.zip
+ unzip customization.zip
+ rm -rf customization.zip
+ read -n 1 -s -r -p "Download Complete--Press Enter To Continue"
+ bash /data/.ToolKit/tools.sh
+ fi
+ if [ $choicewget3 = 2 ]
+ then
+ bash /data/.ToolKit/tools.sh
+ fi
+
+
+
+
+fi
