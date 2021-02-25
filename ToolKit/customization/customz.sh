23d49d83035eeda48e014eeedddebbd50862c34c
DinelMax <73960425+LIGHTNING283@users.noreply.github.com>
2021-02-26 00:35:47 +0700

    Removed Options for leena launcher
    it need more work and hopefully released in the next update. : )

--- ToolKit/customization/customz.sh
+++ ToolKit/customization/customz.sh
@@ -5,7 +5,7 @@ figlet -w $(tput cols) -c "Customization"; echo
 geco "Need To Customize eh...?\n"
 geco "${GREEN}1.New Font + emojis${RC}"
 geco "${GREEN}2.Emojis-v2${RC}"
-geco "${GREEN}3.Leena Launcher${RC}\n"
+#geco "${GREEN}3.Leena Launcher${RC}\n"
 echo -e "\e[93;1m"
 read -p "choose an option : " choice
 echo -e "\e[0m"
@@ -28,19 +28,19 @@ sleep 1
 reboot
 fi
 
-if [ $choice = 3 ];then
-nout rm -rf /data/app/ch.deletescape.lawnchair.plah-1
-nout rm -rf /data/data/ch.deletescape.lawnchair.plah
-nout mkdir /system/app/LeenaLauncher
-nout cp /data/.ToolKit/customization/leena.apk /system/app/LeenaLauncher/
-chmod 777 /system/app/LeenaLauncher/leena.apk
-chmod +x /system/app/LeenaLauncher/leena.apk
-geco "To chage the ui from zoomed it tablet ui to desktop ui click the blue paper plane icon
-then scroll down to find a app named change ui click it once to change the ui to desktop look."
-read -n 1 -s -r -p "Press Enter To Continue"
-sleep 1
-reboot
-fi
+#if [ $choice = 3 ];then
+#nout rm -rf /data/app/ch.deletescape.lawnchair.plah-1
+#nout rm -rf /data/data/ch.deletescape.lawnchair.plah
+#nout mkdir /system/app/LeenaLauncher
+#nout cp /data/.ToolKit/customization/leena.apk /system/app/LeenaLauncher/
+#chmod 777 /system/app/LeenaLauncher/leena.apk
+#chmod +x /system/app/LeenaLauncher/leena.apk
+#geco "To chage the ui from zoomed it tablet ui to desktop ui click the blue paper plane icon
+#then scroll down to find a app named change ui click it once to change the ui to desktop look."
+#read -n 1 -s -r -p "Press Enter To Continue"
+#sleep 1
+#reboot
+#fi
 
 
 
