73aeec9e540285ccf91e91bbc076715bdfce16e0
DinelMax <73960425+LIGHTNING283@users.noreply.github.com>
2021-02-25 16:19:40 +0700

    Update extension.sh

--- gearlock/extension.sh
+++ gearlock/extension.sh
@@ -8,6 +8,7 @@ cp /system/fonts/* /data/.ToolKit/customization/baks/stockfonts/fonts/
 cp -R /data/app/ch.deletescape.lawnchair.plah-1 /data/.ToolKit/customization/baks/launcher/
 cp -R /data/data/ch.deletescape.lawnchair.plah /data/.ToolKit/customization/baks/launcher/
 fi
+clear
 gecpc Welcome
 figlet -w $(tput cols) -c "ToolKit"
 gecpc "TooLKit"; echo 
