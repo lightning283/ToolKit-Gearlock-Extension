56fb0b3622d2d8db515ee106a4299938ae167399
DinelMax <73960425+LIGHTNING283@users.noreply.github.com>
2021-03-09 11:25:37 +0530

    removed GG.

--- ToolKit/apks/apk-install.sh
+++ ToolKit/apks/apk-install.sh
@@ -13,26 +13,16 @@ else
 
 clear
 geco "Choose the Apps you want to install\n"
-geco "${GREEN}1.Game-Gaurdian${RC}"
-geco "${GREEN}2.Lucky-Patcher-Installer${RC}"
-geco "${GREEN}3.Root-Uninstaller${RC}"
-geco "${GREEN}4.EtchDroid${RC}"
-geco "${GREEN}5.Terminal_emu${RC}"
+geco "${GREEN}1.Lucky-Patcher-Installer${RC}"
+geco "${GREEN}2.Root-Uninstaller${RC}"
+geco "${GREEN}3.EtchDroid${RC}"
+geco "${GREEN}4.Terminal_emu${RC}"
 echo -e "\e[93;1m"
 read -p "choose an option : " choice
 echo -e "\e[0m"
 
-if [ $choice = 1 ];then
-nout pm disable com.android.vending
-geco "installing Game Gaurdian..!"
-nout pm install /data/.ToolKit/apks/gg.apk
-nout pm enable com.android.vending
-geco "done..!!";echo 
-read -n 1 -s -r -p "Installed GameGaurdian--Press Enter To Continue"
-bash /data/.ToolKit/tools.sh
-fi
 
-if [ $choice = 2 ];then
+if [ $choice = 1 ];then
 nout pm disable com.android.vending
 geco "installing Lucky patcher installer..!"
 nout pm install /data/.ToolKit/apks/luckypatcher.apk
@@ -44,7 +34,7 @@ fi
 
 
 
-if [ $choice = 3 ];then
+if [ $choice = 2 ];then
 nout pm disable com.android.vending
 geco "installing Root-Uninstaller..!"
 nout pm install /data/.ToolKit/apks/rootuninstaller.apk
@@ -54,7 +44,7 @@ bash /data/.ToolKit/tools.sh
 geco "done..!"
 fi
 
-if [ $choice = 4 ];then
+if [ $choice = 3 ];then
 nout pm disable com.android.vending
 geco "installing EtchDroid..!"
 nout pm install /data/.ToolKit/apks/etchdroid.apk
@@ -67,7 +57,7 @@ bash /data/.ToolKit/tools.sh
 geco "done..!"
 fi
 
-if [ $choice = 5 ];then
+if [ $choice = 4 ];then
 nout pm disable com.android.vending
 geco "installing TerminalEmulator..!"
 nout pm install /data/.ToolKit/apks/termemu.apk
