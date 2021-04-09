45612e1c69716bb511eea381e665161b674a9f77
Xtr126 <80520774+Xtr126@users.noreply.github.com>
2021-04-09 21:21:15 +0530

    Update pubgm-tweaks.sh
    Add gui and remove bad code, fix spelling mistakes

--- ToolKit/game-tweaks/pubgm-tweaks.sh
+++ ToolKit/game-tweaks/pubgm-tweaks.sh
@@ -1,80 +1,102 @@
 #!/gearlock/bin/bash
-clear; sleep 0.1;
-figlet -w $(tput cols) -c "PUBGM-TWEAKS"
-geco "${GREEN}1.Pubgm-crash-fix${RC}"
-geco "${GREEN}2.Pubgm-Guest-ID-Reset${RC}"
-geco "${GREEN}3.Backup Current Guest ID${RC}"
-geco "${GREEN}4.Restore-Backedup-Guest-ID${RC}"
-echo -e "\e[93;1m"
-read -p "choose an option : " choice
-echo -e "\e[0m"
-if [ $choice = 1 ];then
-clear
- geco "${GREEN}1.Method-1${RC}"
- geco "${GREEN}2.Method-2${RC}"
- geco "${GREEN}3.DArkMAtter4.6-webview//Method-3${RC}"
- geco "${GREEN}4.Back${RC}"
- echo -e "\e[93;1m"
- read -p "choose an option : " choice1
- echo -e "\e[0m"
- if [ $choice1 = 1 ];then
- geco "for Pubg kr..\n"
-nout rm -rf /sdcard/Android/data/com.pubg.krmobile/files/ProgramBinaryCache
-nout touch  /sdcard/Android/data/com.pubg.krmobile/files/ProgramBinaryCache
-geco "for Pubg global\n"
-nout rm -rf /sdcard/Android/data/com.tencent.ig/files/ProgramBinaryCache
-nout touch  /sdcard/Android/data/com.tencent.ig/files/ProgramBinaryCache
- read -n 1 -s -r -p "Done..Hope This Helped--Press Enter To Continue"
- bash /data/.ToolKit/tools.sh
- fi
+com_p_cfix() {
+    
+	cfix=(1 "BinaryCache Method"
+    2 "webview_data.lock Method"
+    3 "Install\
+    Darkmatter 4.6 webview")
 
- if [ $choice1 = 2 ];then
- geco "For PUBG KR\n"
-nout rm -rf /data/data/com.pubg.krmobile/app_webview/webview_data.lock
-nout mkdir -p /data/data/com.pubg.krmobile/app_webview/webview_data.lock/
+    HEIGHT=10
+	WIDTH=45
+	CHOICE_HEIGHT=10
+	BACKTITLE=$(gecpc "By SupremeGamers" "_")
+	TITLE="PUBGM Crash fix"
+	MENU="Select which way you want to fix crash"
+	CHOICE=$(dialog --clear --cancel-label "Back" \
+	                --backtitle "$BACKTITLE" \
+	                --title "$TITLE" \
+	                --menu "$MENU" \
+	                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+					"${cfix[@]}" 3>&2 2>&1 1>&3)
+
+    case $CHOICE in
+
+    1)
+geco "for Pubg kr..\n"
+rm -rf /sdcard/Android/data/com.pubg.krmobile/files/ProgramBinaryCache
+touch  /sdcard/Android/data/com.pubg.krmobile/files/ProgramBinaryCache
+geco "for Pubg global\n"
+rm -rf /sdcard/Android/data/com.tencent.ig/files/ProgramBinaryCache
+touch  /sdcard/Android/data/com.tencent.ig/files/ProgramBinaryCache
+dialog --msgbox "Done..Hope This Helped--Press Enter To Continue" 7 45;;
+    2)
+rm -rf /data/data/com.pubg.krmobile/app_webview/webview_data.lock
+ mkdir -p /data/data/com.pubg.krmobile/app_webview/webview_data.lock/
 geco "For PUBG Global\n"
  rm -rf /data/data/com.tencent.ig/app_webview/webview_data.lock
  mkdir -p /data/data/com.tencent.ig/app_webview/webview_data.lock/
- read -n 1 -s -r -p "Done..Hope This Helped--Press Enter To Continue"
- bash /data/.ToolKit/tools.sh
- fi
-
-
- if [ $choice1 = 3 ];then
-geco "Installing DarkMatter-EXo4.6 webview...!!\n"
-nout rm -r /system/priv-app/webview/webview.apk
-nout cp /data/.ToolKit/game-tweaks/webview.apk /system/priv-app/webview/
+dialog --msgbox "Done..Hope This Helped--Press Enter To Continue" 7 45;;
+    3)
+geco "Installing DarkMatter-Exo4.6 webview...!!\n"
+ rm -r /system/priv-app/webview/webview.apk
+ cp /data/.ToolKit/game-tweaks/webview.apk /system/priv-app/webview/
 chmod 777 /system/priv-app/webview/webview.apk
 geco "done..!"
 sleep 1
-reboot
-fi
+stop; start
+;;
+    *)com_main;;
+    esac
 
+}
 
- if [ $choice1 = 4 ];then
- bash /data/.ToolKit/tools.sh
-fi
- fi
-###############################################################################################
-if [ $choice = 2 ];then
-read -n 1 -s -r -p "In Order To this to work you need to log out form the existing guest ID (press enter to continue)"
-nout rm -rf /data/data/com.tencent.ig/shared_prefs/device_id.xml
-read -n 1 -s -r -p "Done..--Press Enter To Continue"
-bash /data/.ToolKit/tools.sh
-fi
-
-if [ $choice = 3 ];then
-nout rm -rf /data/.ToolKit/game-tweaks/guest-id-bak/device_id.xml
+com_gid() {
+dialog --msgbox "In Order To this to work you need to log out from the existing guest ID 
+     (press enter to continue or 
+         ctrl+c to cancel)" 8 45
+read -n 1 -s -r -p 
+ rm -rf /data/data/com.tencent.ig/shared_prefs/device_id.xml
+dialog --msgbox "Done..--Press Enter To Continue" 6 40; com_main
+}
+com_bk_gid() {
+ rm -rf /data/.ToolKit/game-tweaks/guest-id-bak/device_id.xml
 cp /data/data/com.tencent.ig/shared_prefs/device_id.xml /data/.ToolKit/game-tweaks/guest-id-bak/
 cp /data/.ToolKit/game-tweaks/guest-id-bak/device_id.xml /data/data/
-read -n 1 -s -r -p "Done..--Press Enter To Continue"
-bash /data/.ToolKit/tools.sh
-fi
+dialog --msgbox "Done..--Press Enter To Continue" 6 40; com_main
+}
 
-if [ $choice = 4 ];then
-nout rm -rf /data/data/com.tencent.ig/shared_prefs/device_id.xml
+com_rs_gid() {
+rm -rf /data/data/com.tencent.ig/shared_prefs/device_id.xml
 cp /data/.ToolKit/game-tweaks/guest-id-bak/device_id.xml /data/data/com.tencent.ig/shared_prefs/
 chmod 444 /data/data/com.tencent.ig/shared_prefs/device_id.xml
-read -n 1 -s -r -p "Done..--Press Enter To Continue"
-bash /data/.ToolKit/tools.sh
-fi
+dialog --msgbox "Done..--Press Enter To Continue" 6 40; com_main
+}
+
+
+com_main() {
+W=(1 "Pubgm-crash-fix"
+    2 "Pubgm-Guest-ID-Reset"
+    3 "Backup Current Guest ID"
+    4 "Restore-Backedup-Guest-ID")
+
+    HEIGHT=11
+	WIDTH=45
+	CHOICE_HEIGHT=10
+	BACKTITLE=$(gecpc "By SupremeGamers" "_")
+	TITLE="PUBGM Tweaks"
+	MENU="Made with time and passion"
+	CHOICE=$(dialog --clear --cancel-label "Exit" \
+	                --backtitle "$BACKTITLE" \
+	                --title "$TITLE" \
+	                --menu "$MENU" \
+	                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+					"${W[@]}" 3>&2 2>&1 1>&3)
+    case $CHOICE in
+        1)com_p_cfix;;
+        2)com_gid;;
+        3)com_bk_gid;;
+        4)com_rs_gid;;
+        *)bash /data/.ToolKit/tools.sh;;
+    esac
+}
+com_main
