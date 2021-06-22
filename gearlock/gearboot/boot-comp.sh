800ac48859211ee074465cefdb6c14e380040259
riadkabir45 <70858831+riadkabir45@users.noreply.github.com>
2021-06-22 19:26:56 +0600

    Update boot-comp.sh
    refined fx-mount

--- gearlock/gearboot/boot-comp.sh
+++ gearlock/gearboot/boot-comp.sh
@@ -2,9 +2,17 @@
 function mountfx(){
     [ -z "$1" ] && return 0
     [ ! -e "/dev/block/$1" ] && return 0
-    mkdir -p /storage/$1
-    blkid | grep "/dev/block/$1" | grep "ntfs" &&  mount.ntfs /dev/block/$1 /storage/$1  || mount /dev/block/$1 /storage/$1
-    return $?
+    mkdir -p "/storage/$1"
+    fs_type=$(blkid | grep "/dev/block/$1:" | grep -oE "TYPE=\"[^=]+\"" | grep -oE "\".+\"" | sed 's/"//g')
+    case $fs_type in
+        ntfs)
+            mount.ntfs "/dev/block/$1" "/storage/$1"
+            ;;
+	vfat)
+            mount -t vfat "/dev/block/$1" "/storage/$1"
+            ;;
+    esac
+    return "$?"
 }
 
 if [ -e "/data/fx-fstab" ]
